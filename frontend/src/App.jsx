515b96a7c13332804481172a398c38c514f2e11c
fabtrr22 <fabianatorre22@gmail.com>
2024-12-10 00:46:59 -0300

    34 boton whatsapp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
+import WhatsAppButton from "./components/WhatsAppButton/WhatsAppButton";
 
 const App = () => {
+  const phoneNumber = "+1234567890";
+  const defaultMessage = "Hola, tengo una consulta sobre tu producto.";
+
   return (
     <BrowserRouter>
       <AppRouter />
+
+      {/* Botón de WhatsApp en todas las rutas */}
+      <WhatsAppButton phoneNumber={phoneNumber} message={defaultMessage} />
     </BrowserRouter>
   );
 };
