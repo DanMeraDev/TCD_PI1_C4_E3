5b2c91b4fec021ae6e62e85c1406a6e5884a00ea
Diego Quintero <diegoquintero941@gmail.com>
2024-12-14 18:13:37 -0500

    imagen usuario

--- frontend/src/components/Profile/ImageUpload/ProfileImageUpload.jsx
+++ frontend/src/components/Profile/ImageUpload/ProfileImageUpload.jsx
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './ProfileImageUpload.css'; // Archivo CSS actualizado
+import './ProfileImageUpload.css'; // Archivo CSS para estilizar el componente
 
 const ProfileImageUpload = ({ userId, setUserData }) => {
     const [image, setImage] = useState(null);
     const [previewImage, setPreviewImage] = useState(null);
 
+    // Cargar la imagen de perfil al iniciar la sesión
+    useEffect(() => {
+        const savedImageUrl = sessionStorage.getItem("userImageUrl");
+        if (savedImageUrl) {
+            setUserData((prevData) => ({ ...prevData, image: savedImageUrl }));
+        }
+    }, [setUserData]);
+
+    // Manejar la carga de la imagen
     const handleImageUpload = (e) => {
         const imgFile = e.target.files[0];
         if (!imgFile) return;
@@ -15,6 +24,7 @@ const ProfileImageUpload = ({ userId, setUserData }) => {
         setPreviewImage(previewUrl);
     };
 
+    // Manejar la actualización de la imagen de perfil
     const handleImageSubmit = async () => {
         if (image) {
             const formData = new FormData();
@@ -22,13 +32,19 @@ const ProfileImageUpload = ({ userId, setUserData }) => {
 
             try {
                 const token = sessionStorage.getItem("token");
-                const response = await axios.post(`https://ramoja-tours.up.railway.app/api/user/${userId}/profile-image`, formData, {
+                const response = await axios.put(`https://ramoja-tours.up.railway.app/api/user/${userId}/profile-image`, formData, {
                     headers: {
                         Authorization: `Bearer ${token}`,
                     },
                 });
                 if (response.status === 200) {
+                    // Guardar la URL de la imagen en sessionStorage para persistencia
+                    sessionStorage.setItem("userImageUrl", response.data.image);
+
+                    // Actualizar el estado con la nueva URL de la imagen
                     setUserData((prevData) => ({ ...prevData, image: response.data.image }));
+
+                    // Limpiar la vista previa y notificar al usuario
                     setPreviewImage(null);
                     alert("Imagen actualizada exitosamente.");
                 }
