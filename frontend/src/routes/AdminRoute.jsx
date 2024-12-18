db9d23912441ae55c5b88cdd56de67222b5456db
Daniel Mera <dm375211@gmail.com>
2024-12-17 19:06:27 -0500

    admin route redirection fixed

--- frontend/src/routes/AdminRoute.jsx
+++ frontend/src/routes/AdminRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 const AdminRoute = ({ children }) => {
   const isAdmin = sessionStorage.getItem("isAdmin") === "true";
 
-  return isAdmin ? children : <Navigate to="/home" replace />;
+  return isAdmin ? children : <Navigate to="/" replace />;
 };
 
 export default AdminRoute;
